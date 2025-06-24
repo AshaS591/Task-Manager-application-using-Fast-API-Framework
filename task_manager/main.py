d427b7e045d9c0af5cbffcecc2b378dc0bcccf3d
AshaS591 <as1579119@gmail.com>
2025-06-24 17:55:58 +0530

    initialized SQLAlchemy with SQLite engine, session, and declarative base

--- task_manager/main.py
+++ task_manager/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./taskmanager.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
 
 app = FastAPI()
 
@@ -14,5 +22,3 @@ app = FastAPI()
 # @app.post("/ping")
 # def post_test():
 #     return {"message": "POST method works"}
-
-
