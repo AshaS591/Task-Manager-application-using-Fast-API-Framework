43be2e7fe43d4fe874172b1dd587de963b457940
AshaS591 <as1579119@gmail.com>
2025-06-25 16:36:10 +0530

    implemented user model and registration endpoint with database integration
    - Defined SQLAlchemy User model with auto-incrementing ID
    - Created Pydantic schemas for user input (UserCreate) and output (ShowUser)
    - Added /register endpoint to create new users in SQLite DB
    - Included DB session dependency (get_db) and table auto-creation

--- task_manager/main.py
+++ task_manager/main.py
@@ -1,17 +1,46 @@
-from fastapi import FastAPI
-from sqlalchemy import create_engine
+from fastapi import FastAPI,Depends,status
+from sqlalchemy import create_engine,Integer,String,Column
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker,Session
+from pydantic import BaseModel
 
 
+#database creation
 SQLALCHEMY_DATABASE_URL = "sqlite:///./taskmanager.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}) 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True,autoincrement=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+#Database tables creation
 Base.metadata.create_all(bind=engine)
 
-    
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class ShowUser(BaseModel):
+    # id : int
+    username: str
+    class Config:
+        orm_mode = True
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 
 app = FastAPI()
 
@@ -23,6 +52,14 @@ app = FastAPI()
 # def task():
 #     return {'task_name':1}
 
-# @app.post("/ping")
-# def post_test():
-#     return {"message": "POST method works"}
+@app.post('/register',status_code=status.HTTP_200_OK,response_model = ShowUser)
+def create_user(request : UserCreate, db : Session = Depends(get_db)) :
+    new_user = User(id=request.id,username=request.username,hashed_password = request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+    
+
+
+    
